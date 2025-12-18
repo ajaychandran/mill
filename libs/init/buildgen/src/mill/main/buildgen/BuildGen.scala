41592ae3957ff6eaf37880e07406515a910b8085
Ajay Chandran <ajay.chandran.1980@gmail.com>
2025-12-18 20:07:39 +0530

    Updated BuildGen.toScalaIdentifier to exclude reserved names

--- libs/init/buildgen/src/mill/main/buildgen/BuildGen.scala
+++ libs/init/buildgen/src/mill/main/buildgen/BuildGen.scala
@@ -282,7 +282,55 @@ object BuildGen {
   }
 
   private val ScalaIdentifier = "^[a-zA-Z_][\\w]*$".r
-  private def toScalaIdentifier(s: String) = if (ScalaIdentifier.matches(s)) s else s"`$s`"
+  private val reservedNames = Set(
+    "abstract",
+    "case",
+    "catch",
+    "class",
+    "def",
+    "do",
+    "else",
+    "enum",
+    "export",
+    "extends",
+    "false",
+    "final",
+    "finally",
+    "forSome",
+    "for",
+    "given",
+    "if",
+    "implicit",
+    "import",
+    "lazy",
+    "match",
+    "new",
+    "null",
+    "object",
+    "override",
+    "package",
+    "private",
+    "protected",
+    "return",
+    "sealed",
+    "super",
+    "then",
+    "this",
+    "throw",
+    "trait",
+    "try",
+    "true",
+    "type",
+    "val",
+    "var",
+    "while",
+    "with",
+    "yield",
+    "_",
+    "macro"
+  )
+  private def toScalaIdentifier(s: String) =
+    if (ScalaIdentifier.matches(s) && !reservedNames.contains(s)) s else s"`$s`"
 
   private def renderDepsObject(depNames: Seq[(MvnDep, String)]) = {
     s"""package millbuild
